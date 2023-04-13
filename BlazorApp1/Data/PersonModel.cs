c1d7a15c360d34748fa3453141b4a616716a5851
Jonas <Jonas@DESKTOP-VQDLGE9>
2023-04-13 13:42:32 +0200

    Add project files.

---
+++ BlazorApp1/Data/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1.Data
+{
+    public class PersonModel
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        // Create database table called person with these values
+        // the create a stored procedure called usp_GetPerson
+    }
+}
---
+++ BlazorApp1/Services/SqlGetPerson.cs
@@ -0,0 +1,35 @@
+﻿using BlazorApp1.Data;
+using System.Data.SqlClient;
+
+namespace BlazorApp1.Services
+{
+    public class SqlGetPerson
+    {
+        string conn = @"Data Source=localhost\SQLEXPRESS;Database=BlazorApp1;Trusted_Connection=True;";
+        public List<PersonModel> GetPerson()
+        {
+            List<PersonModel> personList = new();
+
+
+            using (SqlConnection connection = new(conn))
+            {
+                connection.Open();
+                using (SqlCommand command = new("EXEC usp_GetPerson", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PersonModel person = new();
+                            person.FName = reader.GetString(0);
+                            person.LName = reader.GetString(1);
+                            person.Birthdate = reader.GetDateTime(2);
+                            personList.Add(person);
+                        }
+                    }
+                }
+            }
+            return personList;
+        }
+    }
+}
